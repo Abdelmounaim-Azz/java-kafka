cecf16b6b99f6992c6c894488648c53ccfa274ed
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-09 05:10:46 +0000

    producer setup

--- src/main/java/simpleapp/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/simpleapp/kafka/tutorial1/ProducerDemo.java
@@ -1,7 +1,27 @@
 package simpleapp.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String bootstrapServers="127.0.0.2:9092";
+        //Create Producer Properties
+        Properties properties=new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        // create the producer
+        KafkaProducer<String,String> producer=new KafkaProducer<String, String>(properties);
+
+        //Create producer record
+        ProducerRecord<String,String> record=new ProducerRecord<String,String>("first-topic","data produced");
+        //Send data
+        producer.send(record);
     }
 }
